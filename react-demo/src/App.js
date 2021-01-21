2a9b07e65dfb0e6f445b306be287b1788cab1e6b
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2021-01-20 22:36:43 -0500

    add react-demo

---
+++ react-demo/src/App.js
@@ -0,0 +1,33 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <head>
+        <title>DEMO:</title>
+        <link rel="stylesheet" href="styles.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.0.0/p5.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.0.0/addons/p5.sound.min.js"></script>
+
+        <script src="https://unpkg.com/ml5@latest/dist/ml5.min.js"></script>
+
+        <script src="sketch.js"></script>
+      </head> */}
+
+      <body>
+        <h1>STEM for FEM</h1>
+
+        <img
+          alt="STEMforFEM.logo.png"
+          src="https://static.wixstatic.com/media/161d0c_92a16877c4b9493bb990f1c1462fa9c0~mv2.png/v1/fill/w_240,h_240,al_c,q_85,usm_0.66_1.00_0.01/STEMforFEM_logo.webp"
+        />
+        <h2>DoodleNet ML5 Demo</h2>
+      </body>
+    </div>
+  );
+}
+
+export default App;
